3c8200cc19ea3cc0cc14efa305e349beef2d55c1
Mark Solomonik <marik.solomonik@gmail.com>
2022-11-19 08:38:10 -0800

    Added ability to send specific file, and removed most of flask output

--- qrdrop/__main__.py
+++ qrdrop/__main__.py
@@ -2,6 +2,10 @@ from flask import Flask, send_file
 import os
 import socket
 import click
+import logging
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 
@@ -11,14 +15,20 @@ s.connect( ('8.8.8.8', 80) )
 localip = s.getsockname()[0]
 s.close()
 
-@app.route('/')
-def transfer_file():
-    return send_file('pyproject.toml')
 
 @click.command
-def launch_file_transfer():
+@click.argument('file', type=click.Path(exists=True, resolve_path=True))
+def launch_file_transfer(file):
     click.echo('hello world')
     os.system('qrencode -t ansiutf8 "http://{}:{}"'.format(localip, port))
+
+    @app.route('/')
+    def transfer_file():
+        click.echo("in transfer_file")
+        click.echo(file)
+        click.echo(click.format_filename(file))
+        return send_file(click.format_filename(file))
+
     app.run(host = '0.0.0.0')
 
 if __name__ == '__main__':
